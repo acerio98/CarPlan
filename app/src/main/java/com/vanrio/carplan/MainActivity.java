1e8ad3bfc7df7bf500c7bbd4a7f52f7c1b1b510e
acerio98 <a.c.riopelle@me.com>
2015-05-16 23:26:43 -0400

    11:26

--- app/src/main/java/com/vanrio/carplan/MainActivity.java
+++ app/src/main/java/com/vanrio/carplan/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -111,6 +112,7 @@ public class MainActivity extends Activity {
                 case 2:
                     return getString(R.string.title_section3).toUpperCase(l);
             }
+
             return null;
         }
     }
@@ -118,7 +120,7 @@ public class MainActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener{
 
         Button newButton;
 
@@ -147,7 +149,15 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.groups_my, container, false);
+            newButton = (Button)rootView.findViewById(R.id.newButton);
             return rootView;
+        }
+
+        @Override
+        public void onClick(View v){
+            if(v.getId() == R.id.newButton){
+                startActivity(new Intent(MainActivity.this, SearchGroupsActivity.class));
+            }
         }
     }
 
